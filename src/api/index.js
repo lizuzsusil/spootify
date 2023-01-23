4c202db4b62ef63b32c68aa72f0bb95356cca83d
acsushil <sushil.ac@amniltech.com>
2023-01-23 10:21:10 +0545

    API call refactor

--- src/api/index.js
+++ src/api/index.js
@@ -3,29 +3,41 @@ import config from '../config';
 
 const { api } = config;
 
-async function getApiData(path,itemType){
-    try{
-      const response = await axios.post(
-        api.authUrl,
-        new URLSearchParams({ 'grant_type': 'client_credentials' }),
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: 'Basic ' + (Buffer.from(api.clientId + ':' + api.clientSecret).toString('base64')),
-          },
-        }
+async function getAccessToken() { 
+  if (localStorage.getItem("access_token")) {
+    return localStorage.getItem("access_token")
+  } 
+  try{
+    const response = await axios.post(
+      api.authUrl,
+      new URLSearchParams({ 'grant_type': 'client_credentials' }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: 'Basic ' + (Buffer.from(api.clientId + ':' + api.clientSecret).toString('base64')),
+        },
+      }
       );
-
-      const resp = await axios.get(
-        `${api.baseUrl}/browse/${path}`,
-          {headers: { Authorization: `Bearer ${response.data.access_token}` }}
-      )
-      
-      return resp?.data[itemType]?.items;
-    } 
-    catch (error) {
+    localStorage.setItem("access_token", response.data.access_token)
+    return(response.data.access_token);
+    } catch (error){
       console.log("error =>", error.message)
     }
 }
 
-export default getApiData;
+
+export default async function getApiData(path,itemType){
+  try{
+      let token = await getAccessToken();
+        
+      const resp = await axios.get(
+        `${api.baseUrl}/browse/${path}`,
+        {headers: { Authorization: `Bearer ${token}` }}
+      )
+      
+      return resp?.data[itemType]?.items;
+    } 
+      catch (error) {
+        console.log("error =>", error.message)
+    }
+}
